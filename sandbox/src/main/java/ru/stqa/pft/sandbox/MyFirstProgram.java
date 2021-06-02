5d20417564e9c0cf17fa6199cbf074542ed24c3d
AnastasiaZar <66090723+AnastasiaZar@users.noreply.github.com>
2021-06-02 10:09:28 -0500

    function
    function

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,24 +1,32 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
+
     public static void main(String[] args) {
-        hello("user");
-        hello ("Eva");
-
-        double l = 5;
-        System.out.println( "squire " + l + " = " + area (l));
-
+        Hello("Eva");
+        Hello("user!");
+    double len = 5;
+        System.out.println("square "+ len + " = " + area (len) );
         double a = 4;
-        double b = 5;
-        System.out.println("squire " + a + " and " + b + " = " + area(a,b));
+        double b = 6;
+        System.out.println(" x " + " = " + area(a, b));
+
     }
-        public static void hello(String somebody) {
-            System.out.println("Hello ,  " + somebody + "!");
-        }
-        public static double area (double len)  {
-        return len * len;
-        }
-        public static double area (double a,double b){
+
+
+    public static void Hello(String smd){
+        System.out.println("Hello , " + smd);
+    }
+
+    public static double area(double l) {
+        return l*l;
+    }
+
+    public static double area(double a, double b){
         return a * b;
-        }
- }
+    }
+}
+
+
+
+
