69d90a66e47481d5bb932b5a23b4fea96582215d
AnastasiaZar <66090723+AnastasiaZar@users.noreply.github.com>
2021-06-03 09:08:26 -0500

    tests start

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,16 +9,25 @@ public class MyFirstProgram {
         Hello("user!");
         Square s = new Square(5);
         System.out.println("square "+ s.l + " = " + s.area () );
+
         Rectangle r = new Rectangle(4,6);
         System.out.println(" rectangle " + " = " + r.area());
+
         Chtoto c = new Chtoto(2,2,2);
         System.out.println(" Chtoto " + " = " +  c.area());
 
+        Point p =new Point(3,4);
+        System.out.println("distance " + " = " + distance(p));
+
+
+
+      }
+
+
+    public static double distance( Point p){
+        return p.p1 * p.p2;
+
     }
-
-
-    
-
 
     public static void Hello(String smd){
         System.out.println("Hello , " + smd);
