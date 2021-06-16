fe21c6f09463e52358a4fed4c5ddbaa8cab1a15a
AnastasiaZar <66090723+AnastasiaZar@users.noreply.github.com>
2021-06-16 09:27:12 -0500

    6/16
    reset password #2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -82,4 +82,23 @@ public class TestBase {
         return false;
       }
     }
+
+    protected void resetpassword() {
+      wd.findElement(By.xpath("(//input[@type='password'])[5]")).click();
+      wd.findElement(By.xpath("(//input[@type='password'])[5]")).clear();
+      wd.findElement(By.xpath("(//input[@type='password'])[5]")).sendKeys("123456");
+      wd.findElement(By.xpath("(//input[@type='password'])[6]")).click();
+      wd.findElement(By.xpath("(//input[@type='password'])[6]")).clear();
+      wd.findElement(By.xpath("(//input[@type='password'])[6]")).sendKeys("123456");
+      wd.findElement(By.xpath("(//button[@type='button'])[10]")).click();
+    }
+
+    protected void usersearch() {
+      wd.findElement(By.linkText("Users")).click();
+      wd.findElement(By.xpath("//div/div/input")).click();
+      wd.findElement(By.xpath("//div/div/input")).clear();
+      wd.findElement(By.xpath("//div/div/input")).sendKeys("ananew");
+      wd.findElement(By.xpath("//div[@id='dashboard-users']/div[2]/table/tbody/tr/td[3]")).click();
+      wd.findElement(By.xpath("//div[5]/button[2]")).click();
+    }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserResetPasword.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserResetPasword.java
@@ -14,23 +14,9 @@ public class UserResetPasword  extends TestBase{
   @Test
   public void testUserResetPasword() throws Exception {
 
-    wd.findElement(By.linkText("Users")).click();
-    wd.findElement(By.xpath("//div/div/input")).click();
-    wd.findElement(By.xpath("//div/div/input")).clear();
-    wd.findElement(By.xpath("//div/div/input")).sendKeys("ananew");
-    wd.findElement(By.xpath("//div[@id='dashboard-users']/div[2]/table/tbody/tr/td[3]")).click();
-    wd.findElement(By.xpath("//div[5]/button[2]")).click();
-    wd.findElement(By.xpath("(//input[@type='password'])[5]")).click();
-    wd.findElement(By.xpath("(//input[@type='password'])[5]")).clear();
-    wd.findElement(By.xpath("(//input[@type='password'])[5]")).sendKeys("123456");
-    wd.findElement(By.xpath("(//input[@type='password'])[6]")).click();
-    wd.findElement(By.xpath("(//input[@type='password'])[6]")).clear();
-    wd.findElement(By.xpath("(//input[@type='password'])[6]")).sendKeys("123456");
-    wd.findElement(By.xpath("(//button[@type='button'])[10]")).click();
+    usersearch();
+    resetpassword();
   }
-
-
-
 
 
 }
